e1784e4dd64947263c21a1c00c513cc5d13f598b
Gustavo Daniel Vitor <97047324+gustavodvitor@users.noreply.github.com>
2023-05-23 12:10:27 -0300

    Add files via upload

--- IdentificadordeIdade/ex02.js
+++ IdentificadordeIdade/ex02.js
@@ -1,21 +1,61 @@
-function carregar(){
-    var msg = window.document.getElementById("msg");
-    var img = window.document.getElementById("imagem");
-    var data = new Date()
-    //var hora = data.getHours()
-    var hora = 22
-    msg.innerHTML = `Agora são ${hora} horas`
-    if (hora >= 0 && hora < 12) {
-        //Bom dia
-        img.src = "img/Manhã.png";
-        document.body.style.background = "#FDE29C";
-    } else if (hora >= 12 && hora <=18){
-        //Boa tarde
-        img.src = 'img/Tarde.png'
-        document.body.style.background = "#FEC586";
-    }else {
-        //Boa noite
-        img.src = 'img/Noite.png'
-        document.body.style.background = "#1D0F05";
-    }
+function verificar(){
+
+    
+var data = new Date()
+var ano = data.getFullYear()
+var fano = window.document.getElementById('txtano')
+var res = window.document.getElementById('res')
+if(fano.value.length == 0 || fano.value > ano)
+{
+window.alert('[ERRO] Verifique os dados e tente novamente!')
+}
+else{
+var fsex = document.getElementsByName('radsex')
+var idade = ano - Number(fano.value)
+var genero = ''
+var img = document.createElement('img')
+img.setAttribute('id', 'foto')
+
+if(fsex[0].checked){
+genero = 'Homem'
+
+if(idade >=0 && idade <10){
+//Criança
+img.setAttribute('src', 'img./criançamenino.jpg')
+}
+else if( idade < 21){
+//Jovem
+img.setAttribute('src', 'img./JovemHomem.jpg')
+}
+else if(idade < 50){
+//Adulto
+img.setAttribute("src", 'img./adultoHomem.jpg')
+}
+else{
+//Idoso
+img.setAttribute('src', 'img./idoso.jpg')
+}
+}
+
+else if( fsex[1].checked){
+genero = 'Mulher'
+
+if (idade >= 0 && idade < 10) {
+//Criança
+img.setAttribute('src', 'img./criançamenina.jpg')
+} else if (idade < 21) {
+//Jovem
+img.setAttribute('src', 'img./JovemMulher.jpg')
+} else if (idade < 50) {
+//Adulto
+img.setAttribute('src', 'img./AdultaMulher.jpg')
+} else {
+//Idoso
+img.setAttribute('src', 'img./idosa')
+}
+}
+res.style.textAlign = 'center'
+res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+res.appendChild(img)
+}
 }
